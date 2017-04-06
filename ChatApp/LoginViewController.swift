116d192f97be05ad27ac55acab5eb2471a6f8b86
Xiaoming Chen <xmchen24@gmail.com>
2017-04-05 21:21:29 -0500

    second commit

---
+++ ChatApp/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  ChatApp
+//
+//  Created by Xiaoming Chen on 4/5/17.
+//  Copyright © 2017 Xiaoming Chen. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginAnonymouslyDidTapped(_ sender: Any) {
+        print("login anonymously test")
+    }
+
+    @IBAction func googleLoginDidTapped(_ sender: Any) {
+        print("google login test")
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
